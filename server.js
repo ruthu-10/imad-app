fee2700886629b3bfec1479673376b1609b7d4ba
ruthu-10 <36703544+ruthu-10@users.noreply.github.com>
2018-03-08 17:17:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-two', function (req,res){
-    res.send('Article two is requested and will be served');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
